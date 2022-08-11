b1e1b9b81bbf08edcc172fde415523aa4de64a78
Ayo Osho <aosho44@gmail.com>
2022-08-11 20:00:30 +0100

    Add testing tutorials

--- app/index.js
+++ app/index.js
@@ -1,1 +1,5 @@
 export const add2Nums = (num1, num2) => num1 + num2;
+
+export const greet = (name) => `Hello ${name}`;
+
+export const calculateTip = (bill, tipRate = 20) => (bill * tipRate) / 100;
--- app/index.test.js
+++ app/index.test.js
@@ -1,5 +1,23 @@
-import { add2Nums } from "./index.js";
+import { add2Nums, calculateTip, greet } from "./index.js";
 
 test("1 + 1 = 2", () => {
   expect(add2Nums(1, 1)).toBe(2);
+});
+
+describe("greet", () => {
+  test("it greets with the correct name", () => {
+    expect(greet("Ayo")).toBe("Hello Ayo");
+    expect(greet("Osho")).toBe("Hello Osho");
+  });
+});
+
+describe("calculateTip", () => {
+  it("calculates a $10 tip on a $100 tab at 10%", () => {
+    expect(calculateTip(100, 10)).toBe(10);
+    expect(calculateTip(100, 20)).toBe(20);
+  });
+
+  it("calculate with a default tip of 20%", () => {
+    expect(calculateTip(100)).toBe(20);
+  });
 });
